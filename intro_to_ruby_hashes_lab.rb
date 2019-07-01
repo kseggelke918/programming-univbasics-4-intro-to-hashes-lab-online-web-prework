1ebdf571b1d1914d388e2549b5a1fbd6f8665665
Katie Seggelke <katie.seggelke@gmail.com>
2019-07-01 16:31:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,10 @@ end
 def update_counting_hash(hash, key)
  if hash[key]
    hash[key]+=1 
+   return hash[key]
  else 
    hash[key] = 1
-   return 
+   return hash[key]
  end 
  
  # given a hash an a key as parameters, return an updated hash
